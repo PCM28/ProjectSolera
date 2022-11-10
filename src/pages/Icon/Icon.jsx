058a5ba467b44283b724a124811cb7fb063f091e
PCM28 <104649488+PCM28@users.noreply.github.com>
2022-11-10 15:20:43 +0100

    Eggs
    New Beginning

--- src/pages/Icon/Icon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Icon.scss'
-function Icon() {
-  return (
-    <div>Icon</div>
-  )
-}
-
-export default Icon
--- src/pages/ListTeam01/ListTeam01.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './ListTeam01'
-function ListTeam01() {
-  return (
-    <div>ListTeam01</div>
-  )
-}
-
-export default ListTeam01
