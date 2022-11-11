1b8241bc2f7e33ce10188aa9c3c24a5768522299
PCM28 <104649488+PCM28@users.noreply.github.com>
2022-11-11 09:56:56 +0100

    "New Beginning 2"

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,48 +4,50 @@ import './Home.scss'
 function Home() {
   return (
     <div>
-      {/* <div className='mainContainer'>
-        <div className='team'>
-          <h3>Team 1</h3>
-          <h1>0</h1>
+      <div className='mainContainer'>
+        <div className='container'>
+          <div className='team'>
+            <h3>Team 1</h3>
+            <h3>0</h3>
+          </div>
+          <div className='team'>
+            <h3>Team 1</h3>
+            <h3>0</h3>
+          </div>
+          <div className='team'>
+            <h3>Team 1</h3>
+            <h3>0</h3>
+          </div>
+          <div className='team'>
+            <h3>Team 1</h3>
+            <h3>0</h3>
+          </div>
+          <div className='team'>
+            <h3>Team 1</h3>
+            <h3>0</h3>
+          </div>
+          <div className='team'>
+            <h3>Team 1</h3>
+            <h3>0</h3>
+          </div>
+          <div className='team'>
+            <h3>Team 1</h3>
+            <h3>0</h3>
+          </div>
+          <div className='team'>
+            <h3>Team 1</h3>
+            <h3>0</h3>
+          </div>
+          <div className='team'>
+            <h3>Team 1</h3>
+            <h3>0</h3>
+          </div>
+          <div className='team'>
+            <h3>Team 1</h3>
+            <h3>0</h3>
+          </div>
         </div>
-        <div className='team'>
-          <h3>Team 2</h3>
-          <h1>0</h1>
-        </div>
-        <div className='team'>
-          <h3>Team 3</h3>
-          <h1>0</h1>
-        </div>
-        <div className='team'>
-          <h3>Team 4</h3>
-          <h1>0</h1>
-        </div>
-        <div className='team'>
-          <h3>Team 5</h3>
-          <h1>0</h1>
-        </div>
-        <div className='team'>
-          <h3>Team 6</h3>
-          <h1>0</h1>
-        </div>
-        <div className='team'>
-          <h3>Team 7</h3>
-          <h1>0</h1>
-        </div>
-        <div className='team'>
-          <h3>Team 8</h3>
-          <h1>0</h1>
-        </div>
-        <div className='team'>
-          <h3>Team 9</h3>
-          <h1>0</h1>
-        </div>
-        <div className='team'>
-          <h3>Team 10</h3>
-          <h1>0</h1>
-        </div>
-      </div> */}
+      </div>
     </div>
     
   )
