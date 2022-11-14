156885e212a73b0741561c5377d32a12e7ba3d53
JoseMartinCalvo <117278127+JoseMartinCalvo@users.noreply.github.com>
2022-11-14 12:48:10 +0100

    El pato
    el pagina Not Found

--- client/src/pages/NotFound/NotFound.jsx
+++ client/src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import './NotFound.scss'
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div>
+      <div className='image'></div>
+      <div className='notFound'>
+        <h1>Not Found</h1>
+      </div>
+      
+    </div>
   )
 }
 
