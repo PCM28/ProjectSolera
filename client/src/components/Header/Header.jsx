a35e84e27b6d3499c933df59b0d3b24e250ee440
dancasnar1 <dancasnar1@alum.us.es>
2022-11-14 11:31:10 +0100

    CSS
    Add the style of home page

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import './Header.scss'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './Header.scss';
+import { Link } from 'react-router-dom';
+import homeButton from '../../images/buttonHomeWhite-removebg-preview.png';
+
 function Header() {
   return (
     <div className='Head'>
         <h1>SOLERA TEAMS. BOOTCAMP 4</h1>
-        <Link to="/"><img src="https://cdn-icons-png.flaticon.com/512/25/25694.png" alt="img" srcSet="" /></Link>
+        <Link to="/"><img src={homeButton} alt="img" srcSet="" /></Link>
       </div>
   )
 }
