74cca31abff2d316c6cba753da7872af307ea771
dancasnar1 <dancasnar1@alum.us.es>
2022-11-15 14:25:59 +0100

    Code Refactoring
    Code Refactoring

--- client/src/pages/ListTeams/ListTeam01/ListTeam.jsx
+++ client/src/pages/ListTeams/ListTeam01/ListTeam.jsx
@@ -6,7 +6,7 @@ function ListTeam01() {
     rows.push(<tr>
     <td>Trabajo en equipo 1</td>
     <td>0 puntos</td>
-    <td><button className='edit'><img src="https://cdn-icons-png.flaticon.com/128/1827/1827933.png" height="40" width="40px"/></button><button className='delete'><img src="https://cdn-icons-png.flaticon.com/128/3405/3405244.png" height="40" width="40px"/></button></td>
+    <td><button className='edit'><img className="button" src="https://cdn-icons-png.flaticon.com/128/1827/1827933.png" alt=""/></button><button className='delete'><img className="button" src="https://cdn-icons-png.flaticon.com/128/3405/3405244.png" alt=""/></button></td>
 </tr>);
     
   }
@@ -16,7 +16,7 @@ function ListTeam01() {
         <tr>
             <th className='title'>TEAM1</th>
             <th className='title'>10 puntos total</th>
-            <th><button className='add' type='button'> <img className='addButton' src="https://cdn-icons-png.flaticon.com/128/1237/1237946.png" height="43" width="40px"/></button></th>
+            <th><button className='add' type='button'> <img className='addButton' src="https://cdn-icons-png.flaticon.com/128/1237/1237946.png" alt="" /></button></th>
         </tr>
       </thead>
       <tbody>
