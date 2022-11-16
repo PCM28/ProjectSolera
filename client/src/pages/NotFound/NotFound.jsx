914825be43ea3ec32f897221e9e0cc5f5313242f
Rubén Palomo Fontán <ruben.palomof@gmail.com>
2022-11-16 09:52:41 +0100

    sound of grilleison added

--- client/src/pages/NotFound/NotFound.jsx
+++ client/src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import './NotFound.scss'
+import sound from '../../assets/audio/grilleison.mp3';
+
 function NotFound() {
+  const audio = new Audio(sound);
+  let isPlayed = false;
+
+  function PlaySound() {
+    if(!isPlayed) {
+      audio.play();
+      isPlayed = true;
+    }
+  };
+
   return (
-    <div>
+    <div onMouseEnter={PlaySound}>
       <div className='image'></div>
       <div className='notFound'>
         <h1>Not Found</h1>
