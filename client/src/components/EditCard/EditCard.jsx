841a2200e8a494abc079d6fc42a117ce255436c3
JoseMartinCalvo <117278127+JoseMartinCalvo@users.noreply.github.com>
2022-11-23 12:26:25 +0100

    EditCard Bien

--- client/src/components/EditCard/EditCard.jsx
+++ client/src/components/EditCard/EditCard.jsx
@@ -1,43 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './EditCard.scss';
 import editLogo from '../../assets/icons/edit-icon/edit48.png';
 import deleteLogo from '../../assets/icons/delete-icon/del48.png';
-import './EditCard.scss';
 
 function EditCard() {
-  // const [editTeam, setEditTeam] = useState(false);
-  // const editable = props.editable;
   return (
-    <div>
-      {/* {editTeam && (
-        <EditDeleteTeam
-          id=""
-          teamName=""
-          onDiscard={() => {
-            setEditTeam(false);
-          }}
-        />
-      )} */}
-      <div className="editCardPosition">
-        <div className="iconContainer">
-          <img
-            className="editButton"
-            src={editLogo}
-            height="40px"
-            width="40px"
-            alt="addLogo"
-          />
-        </div>
-        <div className="iconContainer">
-          <img
-            className="deleteButton"
-            src={deleteLogo}
-            height="43px"
-            width="40px"
-            alt="addLogo"
-          />
+    <Link
+      className="linkAddTeamCard-editable"
+      onClick={() => {
+        // setEditTeam(true);
+      }}
+    >
+      <div className="cardTeamPosition iconCard">
+        {/* <h3 className="teamName">Add Team</h3> */}
+
+        <div className="circular">
+          <div className="editIcon">
+            <img
+              className="editTeamButton"
+              src={editLogo}
+              height="43"
+              width="40px"
+              alt="addLogo"
+            />
+          </div>
+          <div className="deleteIcon">
+            <img
+              className="deleteTeamButton"
+              src={deleteLogo}
+              height="43"
+              width="40px"
+              alt="addLogo"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
